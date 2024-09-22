b9a001e06185c0689fc572abeee3f5505014970a
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-09-22 18:39:19 +0500

    Added userRouter

--- api/index.js
+++ api/index.js
@@ -1,18 +1,20 @@
-import express from 'express';
-import mongoose from 'mongoose';
 import dotenv from 'dotenv'
 dotenv.config();
-
+import express from 'express';
+import mongoose from 'mongoose';
+import userRouter from './routes/user.route.js';
 const app = express();
 
-mongoose.connect(process.env.MONGO).then(() => {
+mongoose.connect(process.env.MONGO_URL).then(() => {
     console.log('MongoDB connected successfully!');
 }).catch(() => {
     console.error('Error connecting to MongoDB!');
 });
 
 
-
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
-})
+});
+
+
+app.use("/api/user", userRouter); 
