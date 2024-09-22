08186d592aae18a62d4fe4aac21fa1e9e6001786
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-09-22 17:25:07 +0500

    Connected to database

--- api/index.js
+++ api/index.js
@@ -1,5 +1,17 @@
 import express from 'express';
+import mongoose from 'mongoose';
+import dotenv from 'dotenv'
+dotenv.config();
+
 const app = express();
+
+mongoose.connect(process.env.MONGO).then(() => {
+    console.log('MongoDB connected successfully!');
+}).catch(() => {
+    console.error('Error connecting to MongoDB!');
+});
+
+
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
