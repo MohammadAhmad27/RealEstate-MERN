3a7d06d4f2427d944bb963f4e368c01448b55b75
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-09-22 19:50:47 +0500

    Updated database connectivity and added authRouter

--- api/index.js
+++ api/index.js
@@ -3,18 +3,26 @@ dotenv.config();
 import express from 'express';
 import mongoose from 'mongoose';
 import userRouter from './routes/user.route.js';
+import authRouter from './routes/auth.route.js'
 const app = express();
+app.use(express.json());
 
-mongoose.connect(process.env.MONGO_URL).then(() => {
-    console.log('MongoDB connected successfully!');
-}).catch(() => {
-    console.error('Error connecting to MongoDB!');
-});
 
+//Connecting to MongoDB 
+main().then(() => {
+    console.log("Connected to MongoDB!");
+})
+    .catch((err) => {
+        console.log(err);
+    });
+async function main() {
+    await mongoose.connect(process.env.MONGO_URL);
+}
 
 app.listen(3000, () => {
     console.log('Server is running on port 3000!');
 });
 
 
-app.use("/api/user", userRouter); 
+app.use("/api/user", userRouter);
+app.use("/api/auth", authRouter);
