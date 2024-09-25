48c0123668ea3ee85220efc70ff82f0be07a5faa
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-09-25 22:47:48 +0500

    Added PrivateRoute component to App.jsx and modified Header.jsx to display user avatar if logged in

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -6,17 +6,22 @@ import SignIn from './pages/SignIn';
 import SignUp from './pages/SignUp';
 import Profile from './pages/Profile';
 import Header from './components/Header';
+import PrivateRoute from './components/PrivateRoute';
 
 export default function App() {
   return (
     <BrowserRouter>
       <Header />
       <Routes>
-        <Route exact path="/" element={<Home />} />
+        <Route element={<PrivateRoute />} >
+          <Route exact path="/" element={<Home />} />
+        </Route>
         <Route exact path="/about" element={<About />} />
         <Route exact path="/sign-in" element={<SignIn />} />
         <Route exact path="/sign-up" element={<SignUp />} />
-        <Route exact path="/profile" element={<Profile />} />
+        <Route element={<PrivateRoute />} >
+          <Route exact path="/profile" element={<Profile />} />
+        </Route>
       </Routes>
     </BrowserRouter>
   )
--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -1,8 +1,10 @@
 import React from 'react'
 import { FaSearch } from "react-icons/fa";
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { useSelector } from 'react-redux';
 
 export default function Header() {
+    const { currentUser } = useSelector((state) => state.user);
     return (
         <header className='bg-slate-200 shadow-md'>
             <nav className='max-w-6xl mx-auto flex justify-between items-center p-3'>
@@ -24,8 +26,14 @@ export default function Header() {
                     <Link to='/about'>
                         <li className='text-slate-700 hover:underline hidden sm:inline'>About</li>
                     </Link>
-                    <Link to='/sign-in'>
-                        <li className='text-slate-700 hover:underline'>Sign in</li>
+                    <Link to='/profile'>
+                        {currentUser ? (
+                            <img src={currentUser.avatar} alt="profile image" />
+                        ) : (
+                            <li className='text-slate-700 hover:underline'>Sign in</li>
+                        )
+                        }
+
                     </Link>
                 </ul>
             </nav>
