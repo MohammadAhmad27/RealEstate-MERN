bab71ffc4e83a889edf1bdd49f3a9d4bc14ff29c
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-09-28 17:29:17 +0500

    Added deleteUser functionality to user controller, route, and client-side profile page, including Redux actions

--- client/src/pages/Profile.jsx
+++ client/src/pages/Profile.jsx
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react'
 import { useRef } from 'react';
 import { getDownloadURL, getStorage, ref, uploadBytesResumable } from 'firebase/storage';
 import { app } from '../Firebase';
-import { updateUserStart, updateUserSuccess, updateUserFailure } from '../redux/user/userSlice'
+import { updateUserStart, updateUserSuccess, updateUserFailure, deleteUserStart, deleteUserSuccess, deleteUserFailure } from '../redux/user/userSlice'
 import { useSelector, useDispatch } from 'react-redux'
 // firebase storage rules
 // allow read;
@@ -78,6 +78,24 @@ export default function Profile() {
 
   }
 
+  const handleDeleteUser = async () => {
+    try {
+      dispatch(deleteUserStart());
+      const res = await fetch(`/api/user/delete/${currentUser._id}`, {
+        method: "DELETE",
+      });
+      const data = await res.json();
+      console.log(data);
+      if (data.success === false) {
+        dispatch(deleteUserFailure(data.message));
+        return;
+      }
+      dispatch(deleteUserSuccess(data));
+    } catch (error) {
+      dispatch(deleteUserFailure(error.message));
+    }
+  }
+
   return (
     <div className='mx-auto max-w-lg p-3'>
       <h1 className='text-3xl font-semibold text-center my-7'>
@@ -108,7 +126,7 @@ export default function Profile() {
         {/* <button className='bg-green-700 text-white p-3 rounded-lg hover:opacity-95 disabled:opacity-80'>Create Listing</button> */}
       </form>
       <div className="flex justify-between mt-5">
-        <span className='text-red-700 cursor-pointer'>Delete Account</span>
+        <span onClick={handleDeleteUser} className='text-red-700 cursor-pointer'>Delete Account</span>
         <span className='text-red-700 cursor-pointer'>Sign In</span>
       </div>
       <p className='text-red-700 mt-5'>{error ? error : ''}</p>
