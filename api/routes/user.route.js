5b3e7b28d657e4c9067a681b60f18fca25449bd5
Mohammad Ahmad <ahmadabbasi5936@gmail.com>
2024-10-04 15:11:22 +0500

    Added getUser endpoint to user controller and route, and updated Listing component to render Contact component

--- api/routes/user.route.js
+++ api/routes/user.route.js
@@ -1,10 +1,11 @@
 import express from "express";
-import { deleteUser, getUserListings, updateUser } from '../controllers/user.controller.js';
+import { deleteUser, getUserListings, updateUser, getUser } from '../controllers/user.controller.js';
 import { verifyToken } from "../utils/verifyUser.js";
 const router = express.Router();
 
 router.put("/update/:id", verifyToken, updateUser);
 router.delete("/delete/:id", verifyToken, deleteUser);
-router.get("/listings/:id", verifyToken, getUserListings)
+router.get("/listings/:id", verifyToken, getUserListings);
+router.get("/:id", verifyToken, getUser);
 export default router;
 
